e699b2f48739cd6dc5ad9953a59c0d4f8ea1445b
Santiago Sosa <santiagososamoressi@gmail.com>
2023-08-24 19:40:38 -0300

    fix behave :p

--- app/__init__.py
+++ app/__init__.py
@@ -6,5 +6,10 @@ def create_app(config_name='development'):
     app.config.from_object(config[config_name])
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     config[config_name].init_app(app)
+    
+    @app.route("/")
+    def hello_world():
+      return "<p>Hello, World!</p>"
+  
 
     return app
