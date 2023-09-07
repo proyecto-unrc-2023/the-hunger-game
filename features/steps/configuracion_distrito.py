e38549f2d70acdf87f453940f7bbf3ec3033935f
Manuel Fernandez <84973959+fer-2001@users.noreply.github.com>
2023-09-07 13:32:47 -0300

    add feature for configuracion-Distrito. Ref #5

---
+++ features/steps/configuracion_distrito.py
@@ -0,0 +1,72 @@
+from flask import url_for
+from behave import given, when, then
+
+
+@given(u'que un usuario aumenta una stat a su distrito')
+def step_impl(context):
+    pass
+
+
+@when(u'da click en el botón + da la estadistica X')
+def step_impl(context):
+    pass
+
+
+@when(u'tiene puntos disponibles para consumir')
+def step_impl(context):
+    pass
+
+
+@then(u'se incrementa un punto la estadística X')
+def step_impl(context):
+    pass
+
+
+@then(u'se decrementa un punto a la cantidad de puntos disponibles')
+def step_impl(context):
+    pass
+
+
+@given(u'que un usuario decrementa una stat a su distrito')
+def step_impl(context):
+    pass
+
+
+@when(u'da click en el botón - de una estadística X')
+def step_impl(context):
+    pass
+
+
+@when(u'X fue incrementada al menos una vez')
+def step_impl(context):
+    pass
+
+
+@then(u'decrementa un punto la estadística  X')
+def step_impl(context):
+    pass
+
+
+@then(u'se incrementa un punto a la cantidad de puntos disponibles')
+def step_impl(context):
+    pass
+
+
+@given(u'que un usuario termina de configura su distrito')
+def step_impl(context):
+    pass
+
+
+@given(u'no hay más puntos disponibles para aumentar las estadísticas')
+def step_impl(context):
+    pass
+
+
+@when(u'el usuario hace click en Comenzar')
+def step_impl(context):
+    pass
+
+
+@then(u'se inicializa el tablero y comienza la simulación')
+def step_impl(context):
+    pass
