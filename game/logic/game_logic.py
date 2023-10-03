13de7072bec4df68cd5b79e8e448b12bc143397e
fer-2001 <fernandezmanuelalumno@gmail.com>
2023-10-03 18:34:01 -0300

    Logic of end_game. Ref. #50

--- game/logic/game_logic.py
+++ game/logic/game_logic.py
@@ -2,6 +2,7 @@ from enum import Enum
 
 from game.logic.board import Board
 from game.logic.cell import State
+from game.logic.district import District
 from game.logic.item import Weapon
 
 
@@ -12,7 +13,7 @@ class GameMode(Enum):
 
 
 class GameLogic:
-    
+
     # Initializes the GameLogic instance.
     def __init__(self):
         self.mode = GameMode.NOT_STARTED
@@ -24,32 +25,32 @@ class GameLogic:
     def new_game(self, rows, columns):
         self.board = Board(rows, columns)
         self.mode = GameMode.TRIBUTES_PLACEMENT
-        
+
     # Returns the positions visible to a tribute within a certain range.
     def tribute_vision_pos(self, tribute):
-      visible_positions = []
-      row = tribute.pos[0]
-      column = tribute.pos[1]
+        visible_positions = []
+        row = tribute.pos[0]
+        column = tribute.pos[1]
 
-    # Checks adjacent cells within a 3 radius.
-      for dr, dc in [(-1, 0), (1, 0), (0, -1), (0, 1), (-1, -1), (-1, 1), (1, -1), (1, 1),
-                     (-2, 0), (2, 0), (0, -2), (0, 2), (-2, -1), (-2, 1), (-1, -2), (-1, 2), 
-                     (-2, -2), (-2, 2), (1, -2), (1, 2), (2, -1), (2, 1), (2, -2), (2, 2),
-                     (-3, 0), (3, 0), (0, -3), (0, 3), (-3, -2), (-3, -1), (-3, 1), (-3, 2),
-                     (-2, -3), (-2, 3), (-1, -3), (-1, 3), (-3, -3), (-3, 3), (1, -3), (1, 3),
-                     (2, -3), (2, 3), (3, -2), (3, -1), (3, 2), (3, 1), (3, -3), (3, 3)]:
-          new_row, new_column = row + dr, column + dc
+        # Checks adjacent cells within a 3 radius.
+        for dr, dc in [(-1, 0), (1, 0), (0, -1), (0, 1), (-1, -1), (-1, 1), (1, -1), (1, 1),
+                       (-2, 0), (2, 0), (0, -2), (0, 2), (-2, -1), (-2, 1), (-1, -2), (-1, 2),
+                       (-2, -2), (-2, 2), (1, -2), (1, 2), (2, -1), (2, 1), (2, -2), (2, 2),
+                       (-3, 0), (3, 0), (0, -3), (0, 3), (-3, -2), (-3, -1), (-3, 1), (-3, 2),
+                       (-2, -3), (-2, 3), (-1, -3), (-1, 3), (-3, -3), (-3, 3), (1, -3), (1, 3),
+                       (2, -3), (2, 3), (3, -2), (3, -1), (3, 2), (3, 1), (3, -3), (3, 3)]:
+            new_row, new_column = row + dr, column + dc
 
-          if 0 <= new_row < self.board.rows and 0 <= new_column < self.board.columns:
-              visible_positions.append((new_row, new_column))
+            if 0 <= new_row < self.board.rows and 0 <= new_column < self.board.columns:
+                visible_positions.append((new_row, new_column))
 
-      return visible_positions
-  
+        return visible_positions
+
     # Returns the list of visible cells for a tribute.
     def tribute_vision_cells(self, tribute):
         if not (0 <= tribute.pos[0] < self.board.rows) or not (0 <= tribute.pos[1] < self.board.columns):
             raise ValueError(f"Coordinates ({tribute.pos[0]}, {tribute.pos[1]}) are out of bounds")
-        
+
         list_pos = self.tribute_vision_pos(tribute)
         tribute_visionCells = []
 
@@ -71,7 +72,7 @@ class GameLogic:
                     distance -= 0.8
             elif cell.get_state() == State.TRIBUTE:
                 if cell.get_tribute().district == None:
-                     distance -= 0.001
+                    distance -= 0.001
 
             return distance
 
@@ -80,8 +81,8 @@ class GameLogic:
         occupied_cells = [
             cell
             for cell in vision_cells
-            if cell.get_state() != State.FREE or (cell.get_state() == State.TRIBUTE and 
-                                                   cell.get_tribute().district != tribute.district)
+            if cell.get_state() != State.FREE or (cell.get_state() == State.TRIBUTE and
+                                                  cell.get_tribute().district != tribute.district)
             # && cell.get_state() != State.TRIBUTE para que sólo se fije en los ítems
             # && cell.get_state() != State.ITEM para que sólo se fije en los tributos
         ]
@@ -92,7 +93,7 @@ class GameLogic:
             return False
 
         return occupied_cells[0]
-    
+
     # Simulates combat between two tributes, moving 'tribute' towards 'tribute2' 
     # and removing 'tribute2' if defeated.
     def fight(self, tribute, tribute2):
@@ -107,8 +108,6 @@ class GameLogic:
             tribute.attack_to(t2, self.board)
             if tribute2.is_dead():
                 self.board.remove_tribute(tribute2)
-
-
 
     # Implements a heuristic move for a character referred to as "tribute" in a game or simulation.
     def heuristic_tribute_first_attempt(self, tribute):
@@ -127,28 +126,49 @@ class GameLogic:
             # Check the state of the cell (ITEM or TRIBUTE).
             if cell.get_state() == State.ITEM:
                 # If it's an item, go to retrieve it.
-                if cell.get_item().pos in self.board.get_adjacent_positions(Tx,Ty):
+                if cell.get_item().pos in self.board.get_adjacent_positions(Tx, Ty):
                     tribute.move_to(x, y, self.board)
                     item = cell.get_item()
                     item.applies_efects(tribute)
                 else:
                     pos = tribute.move_closer_to(x, y, self.board)
                     tribute.move_to(pos[0], pos[1], self.board)
-                    
+
             elif cell.get_state() == State.TRIBUTE:
                 # If it's a tribute,check if its a neutral or not
                 if cell.get_tribute().district == None:
-                    if cell.get_tribute().pos in self.board.get_adjacent_positions(Tx,Ty):
+                    if cell.get_tribute().pos in self.board.get_adjacent_positions(Tx, Ty):
                         tribute.alliance_to(cell.get_tribute())
                     else:
                         pos = tribute.move_closer_to(x, y, self.board)
-                        tribute.move_to(pos[0], pos[1], self.board) 
+                        tribute.move_to(pos[0], pos[1], self.board)
                 else:
                     pos = cell.get_tribute().pos
-                    #move to an adjacent position to it, and if already adjacent, attack.
+                    # move to an adjacent position to it, and if already adjacent, attack.
                     if not (pos in self.board.get_adjacent_positions(tribute.pos[0], tribute.pos[1])):
                         tribute.move_to(pos[0], pos[1], self.board)
                     else:
                         cell_with_tribute = self.board.get_element(x, y)
                         t2 = cell_with_tribute.get_tribute()
-                        self.fight(tribute,t2)
+                        self.fight(tribute, t2)
+
+    # Check which district won (literally return a district)
+    # Return false if still in play
+
+    def end_game(self):
+        district_alive = District()
+        cant_of_districts_alive = 0
+        dead_districts = 0
+        if len(self.districts) == 0:
+            raise ValueError("There is not tributes in the game")
+        else:
+            for aux_district in self.districts:
+                if aux_district.get_cant_tribute() == 0:
+                    dead_districts = dead_districts + 1
+                else:
+                    cant_of_districts_alive = cant_of_districts_alive + 1
+                    if cant_of_districts_alive >= 2:
+                        return False
+                    district_alive = aux_district
+            if cant_of_districts_alive == 1:
+                return district_alive
--- tests/test_game_logic.py
+++ tests/test_game_logic.py
@@ -1,10 +1,12 @@
 import pytest
 
 from game.logic.cell import State
+from game.logic.district import District
 from game.logic.game_logic import GameLogic
 from game.logic.board import Board
 from game.logic.item import Potion, Weapon
 from game.logic.tribute import Tribute
+
 
 @pytest.fixture
 def game():
@@ -25,29 +27,30 @@ def game():
 
 def test_tribute_vision_pos():
     game = GameLogic()
-    game.new_game(7,7)
+    game.new_game(7, 7)
     t1 = Tribute()
-    t1.pos = (3,3)
-        
+    t1.pos = (3, 3)
+
     visible_positions = game.tribute_vision_pos(t1)
     assert len(visible_positions) == 48
     for x in range(6):
-      for y in range(6):
-        if x != 3 and y != 3 :
-            assert (x, y) in visible_positions
-            assert (x, y) in visible_positions
-            assert (x, y) in visible_positions
-            assert (x, y) in visible_positions
-            assert (x, y) in visible_positions
-            assert (x, y) in visible_positions
-            assert (x, y) in visible_positions
+        for y in range(6):
+            if x != 3 and y != 3:
+                assert (x, y) in visible_positions
+                assert (x, y) in visible_positions
+                assert (x, y) in visible_positions
+                assert (x, y) in visible_positions
+                assert (x, y) in visible_positions
+                assert (x, y) in visible_positions
+                assert (x, y) in visible_positions
+
 
 def test_tribute_vision_pos_with_tribute_in_border():
     game = GameLogic()
-    game.new_game(7,7)
+    game.new_game(7, 7)
     t1 = Tribute()
-    t1.pos = (0,0)
-        
+    t1.pos = (0, 0)
+
     visible_positions = game.tribute_vision_pos(t1)
     assert len(visible_positions) == 15
     assert (0, 1) in visible_positions
@@ -69,10 +72,10 @@ def test_tribute_vision_pos_with_tribute_in_border():
 
 def test_tribute_vision_cells_with_a_cells():
     game = GameLogic()
-    game.new_game(3,3)
+    game.new_game(3, 3)
     x, y = 1, 1
     tribute1 = Tribute()
-    game.board.put_tribute(1,1, tribute1)
+    game.board.put_tribute(1, 1, tribute1)
     tribute_vision_cells = game.tribute_vision_cells(tribute1)
     assert len(tribute_vision_cells) == 8
     assert game.board.get_element(0, 0) in tribute_vision_cells
@@ -84,9 +87,10 @@ def test_tribute_vision_cells_with_a_cells():
     assert game.board.get_element(2, 1) in tribute_vision_cells
     assert game.board.get_element(2, 2) in tribute_vision_cells
 
+
 def test_tribute_vision_cells_with_boundary():
     game = GameLogic()
-    game.new_game(3,3)
+    game.new_game(3, 3)
     x, y = 0, 0
     adjacent_cells = game.board.get_adjacents_cells(x, y)
     assert len(adjacent_cells) == 3
@@ -94,9 +98,10 @@ def test_tribute_vision_cells_with_boundary():
     assert game.board.get_element(1, 0) in adjacent_cells
     assert game.board.get_element(1, 1) in adjacent_cells
 
+
 def test_tribute_vision_cells_with_cells_state_tribute():
     game = GameLogic()
-    game.new_game(3,3)
+    game.new_game(3, 3)
     x, y = 1, 1
     game.board.put_tribute(1, 0, Tribute())
     game.board.put_tribute(0, 1, Tribute())
@@ -124,42 +129,44 @@ def test_get_adjacents_cells_with_invalid_coordinates():
     with pytest.raises(ValueError):
         board.get_adjacents_cells(x, y)
 
+
 def test_get_tribute_closenes_with_neutral_weapon_potion_tribute():
     game = GameLogic()
-    game.new_game(3,3)
-    t1 =  Tribute()
-    t2 =  Tribute()
-    neutro =  Tribute()
+    game.new_game(3, 3)
+    t1 = Tribute()
+    t2 = Tribute()
+    neutro = Tribute()
     w = Weapon()
     p = Potion()
-    t1.set_config_parameters(50,5,1,1)
-    t2.set_config_parameters(50,5,1,2)
-    game.board.put_tribute(1,1,t1)
-    game.board.put_tribute(0,1,t2) #//arriba
-    game.board.put_item(2,1,w)#ABAJO
-    game.board.put_item(1,0,p)#izquierda
-    game.board.put_tribute(1,2,neutro)#derecha
-    assert game.tribute_vision_cells_ocupped_order_by_closeness(t1).pos == (2,1)
+    t1.set_config_parameters(50, 5, 1, 1)
+    t2.set_config_parameters(50, 5, 1, 2)
+    game.board.put_tribute(1, 1, t1)
+    game.board.put_tribute(0, 1, t2)  # //arriba
+    game.board.put_item(2, 1, w)  # ABAJO
+    game.board.put_item(1, 0, p)  # izquierda
+    game.board.put_tribute(1, 2, neutro)  # derecha
+    assert game.tribute_vision_cells_ocupped_order_by_closeness(t1).pos == (2, 1)
     game.board.remove_item(w)
-    assert game.tribute_vision_cells_ocupped_order_by_closeness(t1).pos == (1,0)
+    assert game.tribute_vision_cells_ocupped_order_by_closeness(t1).pos == (1, 0)
     game.board.remove_item(p)
-    assert game.tribute_vision_cells_ocupped_order_by_closeness(t1).pos == (1,2)
+    assert game.tribute_vision_cells_ocupped_order_by_closeness(t1).pos == (1, 2)
     game.board.remove_tribute(neutro)
-    assert game.tribute_vision_cells_ocupped_order_by_closeness(t1).pos == (0,1)
-    
-    
+    assert game.tribute_vision_cells_ocupped_order_by_closeness(t1).pos == (0, 1)
+
+
 def test_get_tribute_closeness_with_same_district():
     game = GameLogic()
     game.new_game(7, 7)
     t1 = Tribute()
     t2 = Tribute()
-    t1.set_config_parameters(50,5,1,1)
-    t2.set_config_parameters(50,5,1,1)
+    t1.set_config_parameters(50, 5, 1, 1)
+    t2.set_config_parameters(50, 5, 1, 1)
     game.board.put_tribute(3, 3, t1)
     game.board.put_tribute(3, 4, t2)
     p = Potion()
-    game.board.put_item(5,5,p)
-    assert game.tribute_vision_cells_ocupped_order_by_closeness(t1).pos == (3,4)
+    game.board.put_item(5, 5, p)
+    assert game.tribute_vision_cells_ocupped_order_by_closeness(t1).pos == (3, 4)
+
 
 def test_tribute_vision_cells_ocupped_order_by_closeness_empty_board():
     game = GameLogic()
@@ -169,10 +176,12 @@ def test_tribute_vision_cells_ocupped_order_by_closeness_empty_board():
     result = game.tribute_vision_cells_ocupped_order_by_closeness(t1)
     assert result == False
 
+
 def test_tribute_vision_cells_ocupped_order_by_closeness_tributes_only(game):
     t1 = game.board.get_element(2, 2).get_tribute()
     result = game.tribute_vision_cells_ocupped_order_by_closeness(t1).pos
     assert result == (1, 1)  # Closest tribute is at (1, 1)
+
 
 def test_tribute_vision_cells_ocupped_order_by_closeness_items_only():
     game = GameLogic()
@@ -180,11 +189,12 @@ def test_tribute_vision_cells_ocupped_order_by_closeness_items_only():
     t1 = Tribute()
     w1 = Weapon()
     p1 = Potion()
-    game.board.put_tribute(2,2,t1)
-    game.board.put_item(2,3,w1)
-    game.board.put_item(3,2,p1)
+    game.board.put_tribute(2, 2, t1)
+    game.board.put_item(2, 3, w1)
+    game.board.put_item(3, 2, p1)
     result = game.tribute_vision_cells_ocupped_order_by_closeness(t1).pos
-    assert result == (2,3)  # Closest item (Weapon) is at (2,3)
+    assert result == (2, 3)  # Closest item (Weapon) is at (2,3)
+
 
 def test_tribute_vision_cells_ocupped_order_by_closeness_multiple_items(game):
     t1 = game.board.get_element(2, 2).get_tribute()
@@ -192,52 +202,56 @@ def test_tribute_vision_cells_ocupped_order_by_closeness_multiple_items(game):
     game.board.put_item(2, 1, Weapon())
     result = game.tribute_vision_cells_ocupped_order_by_closeness(t1).pos
     assert result == (2, 1)  # Closest item (Weapon) is at (2, 1)
-    
+
+
 def test_fight_2_tributes_and_one_died():
     game = GameLogic()
-    game.new_game(2,2)
+    game.new_game(2, 2)
     t1 = Tribute()
     t2 = Tribute()
-    t1.set_config_parameters(40,25,1,1)
-    t2.set_config_parameters(40,25,1,2)
-    game.board.put_tribute(0,0,t1)
-    game.board.put_tribute(0,1,t2)
-    game.fight(t1,t2)
+    t1.set_config_parameters(40, 25, 1, 1)
+    t2.set_config_parameters(40, 25, 1, 2)
+    game.board.put_tribute(0, 0, t1)
+    game.board.put_tribute(0, 1, t2)
+    game.fight(t1, t2)
     assert t2.life == 15
-    game.fight(t2,t1)
+    game.fight(t2, t1)
     assert t1.life == 15
     game.fight(t1, t2)
     assert t2.is_dead()
-    assert game.board.get_element(0,1).get_state() == State.FREE
-    
+    assert game.board.get_element(0, 1).get_state() == State.FREE
+
+
 def test_heuristic_tribute_move_towards_item():
     game = GameLogic()
     game.new_game(7, 7)
     tribute = Tribute()
     tribute.force = 2
     game.board.put_tribute(3, 3, tribute)
-    
+
     weapon = Weapon()
     game.board.put_item(4, 3, weapon)
 
     game.heuristic_tribute_first_attempt(tribute)
-    
+
     assert tribute.pos == (4, 3)
+
 
 def test_heuristic_tribute_first_attempt_move_towards_tribute():
     game = GameLogic()
     game.new_game(7, 7)
     tribute1 = Tribute()
     tribute2 = Tribute()
-    tribute1.set_config_parameters(50,5,5,1)
-    tribute2.set_config_parameters(50,5,5,2)
+    tribute1.set_config_parameters(50, 5, 5, 1)
+    tribute2.set_config_parameters(50, 5, 5, 2)
     game.board.put_tribute(3, 3, tribute1)
     game.board.put_tribute(4, 3, tribute2)
 
     game.heuristic_tribute_first_attempt(tribute1)
-    
+
     assert tribute1.pos == (3, 3)
     assert tribute2.life == 45
+
 
 def test_heuristic_tribute_first_attempt_move_randomly():
     game = GameLogic()
@@ -246,25 +260,25 @@ def test_heuristic_tribute_first_attempt_move_randomly():
     game.board.put_tribute(3, 3, tribute)
 
     game.heuristic_tribute_first_attempt(tribute)
-    
+
     # El tributo debe haberse movido a una posición adyacente aleatoria
     assert tribute.pos != (3, 3)
     free_adjacents = game.board.random_choice(tribute)
-    assert not (tribute.past_pos in free_adjacents)    
+    assert not (tribute.past_pos in free_adjacents)
 
 
 def test_heuristic_tribute_first_attempt_neutral_potion():
     game = GameLogic()
     game.new_game(2, 2)
-    
+
     # Configuración del tributo principal
     tribute = Tribute()
     tribute.set_config_parameters(50, 5, 25, 1)
     game.board.put_tribute(1, 1, tribute)
-    
+
     # Configuración de tributo neutral (neutro)
     neutro = Tribute()
-    game.board.put_tribute(0, 0, neutro)  
+    game.board.put_tribute(0, 0, neutro)
 
     # Configuración de poción
     p = Potion()
@@ -276,7 +290,7 @@ def test_heuristic_tribute_first_attempt_neutral_potion():
 def test_heuristic_tribute_first_attempt_complex():
     game = GameLogic()
     game.new_game(5, 4)
-    
+
     # Configuración del tributo principal
     tribute = Tribute()
     tribute.set_config_parameters(50, 5, 25, 1)
@@ -285,11 +299,11 @@ def test_heuristic_tribute_first_attempt_complex():
     # Configuración de tributo oponente (t1)
     t1 = Tribute()
     t1.set_config_parameters(10, 5, 1, 2)
-    game.board.put_tribute(1, 3, t1)  
-    
+    game.board.put_tribute(1, 3, t1)
+
     # Configuración de tributo neutral (neutro)
     neutro = Tribute()
-    game.board.put_tribute(4, 3, neutro)  
+    game.board.put_tribute(4, 3, neutro)
 
     # Configuración de arma y poción
     w = Weapon()
@@ -307,10 +321,24 @@ def test_heuristic_tribute_first_attempt_complex():
     game.heuristic_tribute_first_attempt(tribute)
     assert tribute.life == 45
     assert t1.is_dead()
-    assert game.board.get_element(1,3).get_state() == State.FREE
+    assert game.board.get_element(1, 3).get_state() == State.FREE
     game.heuristic_tribute_first_attempt(tribute)
     game.heuristic_tribute_first_attempt(tribute)
     assert tribute.life == 50
     assert tribute.pos == p.pos
     game.heuristic_tribute_first_attempt(tribute)
-    #assert neutro.district == tribute.district
+
+
+def test_end_game():
+    district1 = District()
+    district2 = District()
+    district1.set_config(50, 5, 3, 1, 2)
+    district2.set_config(50, 5, 3, 2, 2)
+    game = GameLogic()
+    game.districts.append(district1)
+    assert game.end_game() is district1  # end_game returns the only district alive
+    game.districts.append(district2)
+    assert game.end_game() is False
+    game2 = GameLogic()
+    with pytest.raises(ValueError):
+        game2.end_game()
