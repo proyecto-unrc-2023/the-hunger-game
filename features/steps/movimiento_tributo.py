680a0b3b1ad510c13b2fa5814314becb6d8ad81f
MaicolBM <maaicolmorenooo@gmail.com>
2023-10-05 20:35:01 -0300

    Add test-parameterized behave for movimiento_tributo

--- features/steps/movimiento_tributo.py
+++ features/steps/movimiento_tributo.py
@@ -1,88 +1,33 @@
-from flask import url_for
-from behave import given, when, then, step
+from behave import given, when, then
 
-# escenario 1
-@given("que un tributo se encuentra en la posición (0, 3)")
+from game.logic.district import District
+from game.logic.game_logic import GameLogic
+from game.logic.tribute import Tribute
+
+
+@given(u'que un juego fue creado')
 def step_impl(context):
+    context.game = GameLogic()
+    context.game.new_game(4, 4)
+    context.t0 = Tribute()
+    context.d0 = District()
+    context.t0.set_config_parameters(50, 8, 2, 0)
+    context.d0.add_tribute(context.t0)
+    context.game.districts.append(context.d0)
+
+
+@given(u'que tributo se encuentra en la posición {f:d} {c:d}')
+def step_impl(context, f, c):
+    context.game.board.put_tribute(f, c, context.t0)
+    assert context.t0.pos == (f, c)
+
+
+@when(u'se mueve hacia "{direccion}"')
+def step_impl(context, direccion):
     pass
 
-@when("el tributo tenga que moverse")
-def step_impl(context):
-    pass
 
-@then("se moverá hacia abajo a la posición (1, 3)")
-def step_impl(context):
-    pass
-
-# escenario 2
-@given("que un tributo se encuentra en la posición (3, 0)")
-def step_impl(context):
-    pass
-
-@when("el tributo se mueva")
-def step_impl(context):
-    pass
-
-@then("se moverá hacia la derecha a la posición (3, 1)")
-def step_impl(context):
-    pass
-
-# escenario 3
-@given("que un tributo se encuentra en la posición (0, 0)")
-def step_impl(context):
-    pass
-
-@when("el tributo tenga que avanzar")
-def step_impl(context):
-    pass
-
-@then("avanzará en diagonal hacia la posición (1, 1)")
-def step_impl(context):
-    pass
-
-# escenario 4
-@given("que un tributo se encuentra en la posición (3, 2)")
-def step_impl(context):
-    pass
-
-@when("el tributo avance")
-def step_impl(context):
-    pass
-
-@then("avanzará hacia arriba a la posición (2, 2)")
-def step_impl(context):
-    pass
-
-# escenario 5
-@given("que un tributo se encuentra en la posición (0, 2)")
-def step_impl(context):
-    pass
-
-@when("cuando realice un movimiento")
-def step_impl(context):
-    pass
-
-@then("se moverá hacia la izquierda a la posición (0, 1)")
-def step_impl(context):
-    pass
-
-# escenario 6
-@given("que un tributo se encuentre en la posición (2, 1)")
-def step_impl(context):
-    pass
-
-@step("no es adyacente a otro tributo")
-def step_impl(context):
-    pass
-
-@step("no se encuentra en la misma celda que un ítem")
-def step_impl(context):
-    pass
-
-@when("se ejecute un paso de iteración")
-def step_impl(context):
-    pass
-
-@then("se moverá a una celda adyacente disponible (1, 1)")
-def step_impl(context):
-    pass
+@then(u'tributo se mueve hacia la posición {res1:d} {res2:d}')
+def step_impl(context, res1, res2):
+    context.t0.move_to(res1, res2, context.game.board)
+    assert context.t0.pos == (res1, res2)
