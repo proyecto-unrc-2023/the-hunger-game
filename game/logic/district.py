a393287dc9af966437ddef454b68c8e70d321401
Cristian <cristiankhan23@gmail.com>
2023-10-06 16:33:25 -0300

    Fix init_game. Ref #83

--- game/logic/district.py
+++ game/logic/district.py
@@ -26,31 +26,29 @@ class District:
             raise ValueError(f'Life can not be negative: {life}')
         if force < 5 or force > 10:
             raise ValueError(f'Force must be between 5 and 10 points: {force}')
-        if alliance < 1 or alliance > 10:
-            raise ValueError(f'Alliance must be between 1 and 10 points: {alliance}')
-        if number_district < 1:
-            raise ValueError(f'Number of district can not be less than 1: {number_district}')
-
-        self.number_district = number_district  # setea el numero del distrito
-        self.cant_tributes = cant_tributes  # setea la cantidad de tributos en el distrito
+        if alliance < 3 or alliance > 10:
+            raise ValueError(f'Alliance must be between 3 and 10 points: {alliance}')
+        
+        self.number_district = number_district
+        self.cant_tributes = cant_tributes
 
         for i in range(self.cant_tributes):
-            trib = Tribute()
-            trib.life = life  # setea la vida
-            trib.force = force  # setea la fuerza
-            trib.alliance = alliance  # setea la alianza
-            trib.district = number_district  # setea el numero del distrito
-            self.tributes.append(trib)  # se agrega el tributo configurado
+            tribute = Tribute()
+            tribute.life = life
+            tribute.force = force
+            tribute.alliance = alliance
+            tribute.district = number_district
+            self.tributes.append(tribute)
 
     # Configure tributes of an district with random stats.
     def set_config_random(self, num_district):
         for i in range(4):
-            trib = Tribute()
-            trib.life = 50
-            trib.force = random.randint(5, 10)
-            trib.alliance = random.randint(1, 10)
-            trib.district = num_district
-            self.tributes.append(trib)  # se agrega el tributo configurado
+            tribute = Tribute()
+            tribute.life = 50
+            tribute.force = random.randint(5, 10)
+            tribute.alliance = random.randint(3, 10)
+            tribute.district = num_district
+            self.tributes.append(tribute)
 
     # Add one tribute in a list of tributes.
     def add_tribute(self, tribute):
