ec4d3460a68f7bba154f19fcde69eec49b9c27c5
Dosaagu <agustind994@gmail.com>
2023-10-16 04:09:56 -0300

    Changes in css and conexion between Menu and Game

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,26 @@
-import './App.css';
-import React from "react";
-import StatsSettings from "./components/Menu.jsx"
+// App.jsx
+import React, { useState } from "react";
+import Menu from "./components/Menu.jsx";
 import Game from "./components/Game.jsx";
 import Header from "./components/Header.jsx";
 
-function App() {
+const App = () => {
+  const [gameStarted, setGameStarted] = useState(false);
+
+  const handleStartGame = () => {
+    setGameStarted(true);
+  };
+
   return (
     <div className="parent">
-      <Header districtChosed={1} districtsLive={4}/>
-      <StatsSettings />
+      <Header districtChosed={1} districtsLive={4} />
+      {!gameStarted ? (
+        <Menu onStartGame={handleStartGame} />
+      ) : (
+        <Game />
+      )}
     </div>
   );
-}
+};
 
 export default App;
