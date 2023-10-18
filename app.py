45df978f15413e88ae145d2838e5e3b09a729159
MaicolBM <maaicolmorenooo@gmail.com>
2023-10-18 19:43:58 -0300

    start connection

--- app.py
+++ app.py
@@ -5,4 +5,7 @@ app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 
 if __name__ == '__main__':
     app.run(debug=True)
-    
+
+    with app.app_context():
+        for rule in app.url_map.iter_rules():
+            print(rule)
---
+++ app/apis/apy.py
@@ -0,0 +1,8 @@
+from flask import Blueprint, jsonify
+
+api_blueprint = Blueprint('api', __name__)
+
+
+@api_blueprint.route('/api/some_endpoint', methods=['GET'])
+def get_data():
+    return jsonify({"data": "Datos de ejemplo"})
