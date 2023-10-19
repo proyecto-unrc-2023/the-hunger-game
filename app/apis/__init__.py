87ab9b89af24f989718b668eb349d2d543032a6c
MaicolBM <maaicolmorenooo@gmail.com>
2023-10-19 20:06:50 -0300

    create tribute schemma.#106

--- app/apis/__init__.py
+++ app/apis/__init__.py
@@ -1,7 +1,10 @@
 from flask import Blueprint
+from flask_restful import Api
 
 # Create a blueprint
 apis_bp = Blueprint('apis', __name__)
+api = Api(apis_bp)
 
 # Import module routes
 from app.apis import routes
+from app.apis import apy
--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 import os
 
+
 class Config(object):
     TESTING = False
 
@@ -7,16 +8,20 @@ class Config(object):
     def init_app(app):
         pass
 
+
 class ProductionConfig(Config):
     SQLALCHEMY_DATABASE_URI = 'mysql://user@localhost/foo'
+
 
 class DevelopmentConfig(Config):
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = "sqlite:////tmp/foo.db"
 
+
 class TestingConfig(Config):
     SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'
     TESTING = True
+
 
 config = {
     'development': DevelopmentConfig,
