a7b1d3d9cd0ace7e5484d0b38f85ad5fd750fb44
Cristian <cristiankhan23@gmail.com>
2023-11-06 18:18:50 -0300

    DB in progres

--- app.py
+++ app.py
@@ -5,9 +5,9 @@ from app import db
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 
 if __name__ == '__main__':
-    
     print(app.url_map)
     app.run(debug=True)
+    
     with app.app_context():
         db.create_all()
         for rule in app.url_map.iter_rules():
